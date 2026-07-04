6015cc626133647c10a80587b44112932211be2d
Santhiya Karuppanan <santhiya24012007@gmail.com>
2026-07-04 13:09:26 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://ai-powered-adaptive-dsa-learning-and-interview-p-production.up.railway.app',,
 })
 
 export const signup = (data) => API.post('/auth/signup', data)
