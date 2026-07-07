bf9597c088399b74b9e0c23f4c5c45115b78954a
Santhiya Karuppanan <santhiya24012007@gmail.com>
2026-07-07 14:52:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
-
+})
 export const signup = (data) => API.post('/auth/signup', data)
 export const login = (data) => API.post('/auth/login', data)
 export const resetPassword = (data) => API.post('/auth/reset-password', data)
