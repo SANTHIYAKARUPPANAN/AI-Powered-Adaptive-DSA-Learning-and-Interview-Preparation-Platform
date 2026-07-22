3221b28d44407d07a4dd799761ffd86551183e1e
Santhiya Karuppanan <santhiya24012007@gmail.com>
2026-07-23 00:41:25 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
+  baseURL: "https://dsa-tracker-backend-35rg.onrender.com"
 })
 
 export const signup = (data) => API.post('/auth/signup', data)
